2f07c3b01b6155e2d9dc2b10eb05767b544b69df
hikellycheng <kelly.c@evolutivelabs.com>
2020-10-06 14:31:38 +0800

    push

--- mysite/ads/getSheet.py
+++ mysite/ads/getSheet.py
@@ -6,13 +6,16 @@ from oauth2client import file, client, tools
 import ads.globals as Var
 import mysite.settings as Settings
 import pandas as pd
+import os
 
 # Setup the Sheets API
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 store = file.Storage('credentials.json')
 creds = store.get()
 if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+    JSON_FILE = os.path.join(Settings.BASE_DIR, 'client_secret.json')
+    print(JSON_FILE)
+    flow = client.flow_from_clientsecrets(JSON_FILE, SCOPES)
     creds = tools.run_flow(flow, store)
 service = build('sheets', 'v4', http=creds.authorize(Http()))
 
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -14,7 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -117,6 +118,3 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
