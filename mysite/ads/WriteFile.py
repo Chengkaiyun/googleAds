5e6a0b073a69b8e877f1ca6f8150eab81d06fbb8
hikellycheng <kelly.c@evolutivelabs.com>
2020-10-20 16:08:09 +0800

    download

--- mysite/ads/WriteFile.py
+++ mysite/ads/WriteFile.py
@@ -3,12 +3,16 @@ from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 
+import mysite.settings as Settings
+
 # Setup the Sheets API
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-store = file.Storage('credentials.json')
+JSON_FILE = os.path.join(Settings.BASE_DIR, "credentials.json")
+print(JSON_FILE)
+store = file.Storage(JSON_FILE)
 creds = store.get()
 if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+    flow = client.flow_from_clientsecrets(JSON_FILE, SCOPES)
     creds = tools.run_flow(flow, store)
 service = build('sheets', 'v4', http=creds.authorize(Http()))
 
